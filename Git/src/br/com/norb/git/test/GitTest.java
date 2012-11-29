78db7cb5dab2d92b25a1ecd6000e4d72ff32013e
edenirnorberto <enanschau@gmail.com>
2012-11-28 22:43:16 -0200

    Test 2

--- Git/src/br/com/norb/git/test/GitTest.java
+++ Git/src/br/com/norb/git/test/GitTest.java
@@ -1,7 +1,7 @@
 package br.com.norb.git.test;
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Git Test...");
+		System.out.println("Git Test.................");
 	}
 
 }
